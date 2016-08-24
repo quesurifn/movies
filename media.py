c671a8672ee67da9b3454360fa59cfd777787751
Kyle <kyle.c.r.fahey@gmail.com>
2016-08-24 13:36:21 -0500

    Update media.py

--- media.py
+++ media.py
@@ -1,6 +1,8 @@
-import webbrowser
+import webbrowser #imports webbrowser to call the function in show_trailer()
 
 class Movie():
+    """This class creates the variables to be called in entertainment_center.py
+    """
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
@@ -10,4 +12,6 @@ class Movie():
         
     
     def show_trailer(self):
+        """This function calls the input of the variable trailer_youtube and opens 
+        it in the default web browser""" 
         webbrowser.open(self.trailer_youtube_url)
